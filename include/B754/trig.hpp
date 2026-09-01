8d2f68eb55642d33511a87700f79d482b8d578f0
ntcd_lol <volovikvladimir30@gmail.com>
2026-09-01 08:15:37 +0600

    Update trig.hpp

--- include/B754/trig.hpp
+++ include/B754/trig.hpp
@@ -1,1 +1,18 @@
+/* -----
+ * Baddy754 -> Trigonometric
+ *
+ * Trigonometric operations.
+ *
+ * Creator: ntcd_lol
+ * License: MIT
+ *
+ * For people by people with love :heart: :3
+ * -----
+ */
 
+#include "B754/basic.hpp"
+
+namespace baddy754::trig {
+    FFunc sin(float x);
+    DFunc sin(double x);
+}
